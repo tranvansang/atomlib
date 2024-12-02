dbdc88f9c627fba1f2883d169b5362a02e1bd464
Sang <t@sang.jp>
2024-12-02 18:43:10 +0900

    support readonly

--- lib/atomlib.dart
+++ lib/atomlib.dart
@@ -14,7 +14,7 @@ class Atom<T> extends ChangeNotifier {
   static T of<T>(BuildContext context) {
     final state = context.dependOnInheritedWidgetOfExactType<AtomInheritedWidget<T>>();
     if (state == null) throw Exception('Must be used within an AtomContainer');
-    return state.state.value;
+    return state.state.value(context);
   }
 }
 
@@ -26,18 +26,45 @@ class AtomInheritedWidget<T> extends InheritedWidget {
 }
 
 class AtomProvider<T> extends StatefulWidget {
-  const AtomProvider({super.key, required this.child, required this.atom});
-  final Atom<T> atom;
   final Widget child;
+  final bool _readonly;
   @override
   createState() => _State<T>();
+  AtomProvider({super.key, required this.child, required this.atom})
+      : create = null,
+        _readonly = false,
+        dispose = null;
+  final Atom<T>? atom;
+  AtomProvider.readonly({super.key, required this.child, required this.create, this.dispose})
+      : atom = null,
+        _readonly = true;
+  final void Function(T value)? dispose;
+  final T Function(BuildContext context)? create;
 }
 
 class _State<T> extends State<AtomProvider<T>> {
-  T get value {}
+  bool _initialized = false;
+  late final T _value;
+  T value(BuildContext context) {
+    if (!widget._readonly) return widget.atom!.value;
+    if (!_initialized) {
+      _value = widget.create!.call(context);
+      _initialized = true;
+    }
+    return _value;
+  }
+
+  @override
+  dispose() {
+    if (_initialized) widget.dispose?.call(_value);
+    super.dispose();
+  }
+
   @override
   build(context) {
-    return ListenableBuilder(
-        listenable: widget.atom, builder: (_, __) => AtomInheritedWidget<T>(state: this, child: widget.child));
+    return widget._readonly
+        ? AtomInheritedWidget<T>(state: this, child: widget.child)
+        : ListenableBuilder(
+            listenable: widget.atom!, builder: (_, __) => AtomInheritedWidget<T>(state: this, child: widget.child));
   }
 }
