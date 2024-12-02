26fcb21c46fb818955e1078bc9405b833ccf089d
Sang <t@sang.jp>
2024-12-02 18:28:42 +0900

    simplify impl

--- lib/atomlib.dart
+++ lib/atomlib.dart
@@ -4,7 +4,6 @@ import 'package:flutter/widgets.dart';
 
 class Atom<T> extends ChangeNotifier {
   Atom(T value) : _value = value;
-
   T _value;
   T get value => _value;
   set value(T value) {
@@ -15,38 +14,30 @@ class Atom<T> extends ChangeNotifier {
   static T of<T>(BuildContext context) {
     final state = context.dependOnInheritedWidgetOfExactType<AtomInheritedWidget<T>>();
     if (state == null) throw Exception('Must be used within an AtomContainer');
-    return state.value;
+    return state.state.value;
   }
 }
 
 class AtomInheritedWidget<T> extends InheritedWidget {
-  final T value;
-
-  const AtomInheritedWidget({required this.value, super.key, required child}) : super(child: child);
-
+  final _State<T> state;
+  const AtomInheritedWidget({required this.state, super.key, required child}) : super(child: child);
   @override
   updateShouldNotify(oldWidget) => true;
 }
 
 class AtomProvider<T> extends StatefulWidget {
-  final Atom<T> atom;
-
   const AtomProvider({super.key, required this.child, required this.atom});
-
+  final Atom<T> atom;
   final Widget child;
-
   @override
   createState() => _State<T>();
 }
 
 class _State<T> extends State<AtomProvider<T>> {
+  T get value {}
   @override
   build(context) {
     return ListenableBuilder(
-        listenable: widget.atom,
-        builder: (_, __) => AtomInheritedWidget<T>(
-              value: widget.atom.value,
-              child: widget.child,
-            ));
+        listenable: widget.atom, builder: (_, __) => AtomInheritedWidget<T>(state: this, child: widget.child));
   }
 }
