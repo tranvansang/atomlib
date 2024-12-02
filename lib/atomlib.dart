dc21c87affa67f0dac5b7963a5cea9e444de21ba
Sang <t@sang.jp>
2024-12-02 18:51:49 +0900

    null safety

--- lib/atomlib.dart
+++ lib/atomlib.dart
@@ -14,45 +14,46 @@ class Atom<T> extends ChangeNotifier {
   static T of<T>(BuildContext context) {
     final state = context.dependOnInheritedWidgetOfExactType<AtomInheritedWidget<T>>();
     if (state == null) throw Exception('Must be used within an AtomContainer');
-    return state.state.value(context);
+    return state.value(context);
   }
 }
 
 class AtomInheritedWidget<T> extends InheritedWidget {
-  final _State<T> state;
-  const AtomInheritedWidget({required this.state, super.key, required child}) : super(child: child);
+  final T Function(BuildContext) value;
+  const AtomInheritedWidget({required this.value, super.key, required super.child});
   @override
   updateShouldNotify(oldWidget) => true;
 }
 
-class AtomProvider<T> extends StatefulWidget {
+class AtomProvider<T> extends StatelessWidget {
+  AtomProvider({super.key, required this.child, required this.atom});
   final Widget child;
-  final bool _readonly;
+  final Atom<T> atom;
+  // AtomProvider.readonly({super.key, required this.child, required this.create, this.dispose});
   @override
-  createState() => _State<T>();
-  AtomProvider({super.key, required this.child, required this.atom})
-      : create = null,
-        _readonly = false,
-        dispose = null;
-  final Atom<T>? atom;
-  AtomProvider.readonly({super.key, required this.child, required this.create, this.dispose})
-      : atom = null,
-        _readonly = true;
-  final void Function(T value)? dispose;
-  final T Function(BuildContext context)? create;
+  build(_) {
+    return ListenableBuilder(
+        listenable: atom,
+        builder: (_, __) => AtomInheritedWidget<T>(
+            value: (_) {
+              return atom.value;
+            },
+            child: child));
+  }
 }
 
-class _State<T> extends State<AtomProvider<T>> {
+class _Readonly<T> extends StatefulWidget {
+  _Readonly({super.key, required this.child, required this.create, this.dispose});
+  final Widget child;
+  final void Function(T value)? dispose;
+  final T Function(BuildContext context) create;
+  @override
+  createState() => _State<T>();
+}
+
+class _State<T> extends State<_Readonly<T>> {
   bool _initialized = false;
   late final T _value;
-  T value(BuildContext context) {
-    if (!widget._readonly) return widget.atom!.value;
-    if (!_initialized) {
-      _value = widget.create!.call(context);
-      _initialized = true;
-    }
-    return _value;
-  }
 
   @override
   dispose() {
@@ -62,9 +63,14 @@ class _State<T> extends State<AtomProvider<T>> {
 
   @override
   build(context) {
-    return widget._readonly
-        ? AtomInheritedWidget<T>(state: this, child: widget.child)
-        : ListenableBuilder(
-            listenable: widget.atom!, builder: (_, __) => AtomInheritedWidget<T>(state: this, child: widget.child));
+    return AtomInheritedWidget<T>(
+        value: (context) {
+          if (!_initialized) {
+            _value = widget.create.call(context);
+            _initialized = true;
+          }
+          return _value;
+        },
+        child: widget.child);
   }
 }
